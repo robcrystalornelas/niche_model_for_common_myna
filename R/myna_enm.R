36689cdff3a1448ba194879e953b0426a5c0fbf4
rpecchia <rpecchia@gmail.com>
2016-02-08 22:00:55 -0500

    removed absence counts

--- R/myna_enm.R
+++ R/myna_enm.R
@@ -13,15 +13,15 @@ data(wrld_simpl)
 
 setwd("/Users/rpecchia/Desktop/Zoogeo Spring 2016/zoogeo-project/R")
 
-#myna_from_gbif <- gbif("Acridotheres", "tristis*", geo = TRUE)
-# '*' also downloads occurrences of subspecies
+myna_from_gbif <- gbif("Acridotheres", "tristis*", geo = TRUE)
+# adding * to species will download all subspecies
 # if geo = TRUE, ensures all records have latitude AND longitude
 
-myna_points <- select(myna_from_gbif, species, lon, lat)
-head(myna_points)
+myna_absence_removed<-myna_from_gbif[!myna_from_gbif$datasetName == "EBCC Atlas of European Breeding Birds", ] #remove absence records
+unique(myna_maybe_no_absence$country) #check to make sure absence records removed
+
+myna_points <- select(myna_absence_removed, species, lon, lat)
 write.csv(myna_points, file = "myna_from_gbif.csv")
 
 plot(wrld_simpl, axes=TRUE, col="light yellow", main = "Occurrences for Common Myna \n(Acridotheres tristis) from GBIF")
-points(myna_points$lon, myna_points$lat, = "blue", cex = .5)
-
-?maxent
+points(myna_points$lon, myna_points$lat ,col = "blue", cex = 1)
