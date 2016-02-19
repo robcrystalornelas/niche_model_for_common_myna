005274259799e75de715c93857855830a2f02910
rpecchia <rpecchia@gmail.com>
2016-02-19 10:09:45 -0500

    Downloaded climate variables

--- R/myna_enm.R
+++ R/myna_enm.R
@@ -13,16 +13,47 @@ data(wrld_simpl)
 
 setwd("/Users/rpecchia/Desktop/Zoogeo Spring 2016/zoogeo-project/R")
 
+####
+
+# Download Occurrences
+
+####
+
 #myna_from_gbif <- gbif("Acridotheres", "tristis*", geo = TRUE)
 # adding * to species will download all subspecies
 # if geo = TRUE, ensures all records have latitude AND longitude
 dim(myna_from_gbif)
-
-myna_no_absences<-subset(myna_from_gbif, datasetName!="EBCC Atlas of European Breeding Birds")
+myna_no_absences<-myna_from_gbif[!grepl("EBCC Atlas of European Breeding Birds", myna_from_gbif$datasetName),] #remove entries from EBCC
 unique(myna_no_absences$country) #check to make sure absence records removed
 myna_points <- select(myna_no_absences, species, lon, lat) #select only necessary columns
-myna_points <- myna_points[complete.cases(myna_points),] #remove any rows where data is NA
+head(myna_points)
+myna_points <- myna_points[complete.cases(myna_points),] #ensure no entries are missing lat/lon/country
 write.csv(myna_points, file = "myna_from_gbif.csv") #write file of species name and occurrence data
 
 plot(wrld_simpl, axes=TRUE, col="light yellow", main = "Occurrences for Common Myna \n(Acridotheres tristis) from GBIF")
-points(myna_points$lon, myna_points$lat ,col = "blue", cex = .75)
+points(myna_points$lon, myna_points$lat ,col = "blue", cex = .35)
+
+####
+
+# filtering out sightings
+
+####
+
+filter(myna_points, lat>(80)) #find northern midwest point
+points(22.01667,88.01667) #confirm this is northernmost point
+which(myna_points$lon == 22.01667, myna_points$lat== 88.01667) #find it in the data.frame
+myna_points<- myna_points[-145375,] #remove that row!
+myna_points<- myna_points[-145453,] #remove that row!
+
+####
+
+# Download environmental data
+
+####
+
+worldclim <- getData('worldclim', var='bio', res=2.5)
+plot(worldclim[[1]], main = "Annual Mean Temp \n and Common Myna Occurrences")
+points(points(myna_points$lon, myna_points$lat ,col = "blue", cex = .25))
+
+plot(worldclim[[19]], main = "Precip in Coldest Quarter \n and Common Myna Occurrences")
+points(points(myna_points$lon, myna_points$lat ,col = "blue", cex = .25))
