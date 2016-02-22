3b122253c90617ab409463e2d2d7e68c65d689f7
rpecchia <rpecchia@gmail.com>
2016-02-22 15:55:36 -0500

    added code for spatial thinning

--- R/myna_enm.R
+++ R/myna_enm.R
@@ -9,6 +9,8 @@
 library(dismo)
 library(dplyr)
 library(maptools)
+library(spThin)
+library(ggplot2)
 data(wrld_simpl)
 
 setwd("/Users/rpecchia/Desktop/Zoogeo Spring 2016/zoogeo-project/R")
@@ -25,13 +27,18 @@ setwd("/Users/rpecchia/Desktop/Zoogeo Spring 2016/zoogeo-project/R")
 dim(myna_from_gbif)
 myna_no_absences<-myna_from_gbif[!grepl("EBCC Atlas of European Breeding Birds", myna_from_gbif$datasetName),] #remove entries from EBCC
 unique(myna_no_absences$country) #check to make sure absence records removed
-myna_points <- select(myna_no_absences, species, lon, lat) #select only necessary columns
+myna_points <- select(myna_no_absences, species, lon, lat, country) #select only necessary columns
 head(myna_points)
 myna_points <- myna_points[complete.cases(myna_points),] #ensure no entries are missing lat/lon/country
+
 write.csv(myna_points, file = "myna_from_gbif.csv") #write file of species name and occurrence data
 
 plot(wrld_simpl, axes=TRUE, col="light yellow", main = "Occurrences for Common Myna \n(Acridotheres tristis) from GBIF")
 points(myna_points$lon, myna_points$lat ,col = "blue", cex = .35)
+
+myna_unique <- unique(myna_points) #returns only unique combinations of myna points
+plot(wrld_simpl, axes=TRUE, col="light yellow", main = "Occurrences for Common Myna \n(Acridotheres tristis) from GBIF")
+points(myna_unique$lon, myna_unique$lat ,col = "blue", cex = .35)
 
 ####
 
@@ -39,11 +46,14 @@ points(myna_points$lon, myna_points$lat ,col = "blue", cex = .35)
 
 ####
 
-filter(myna_points, lat>(80)) #find northern midwest point
+filter(myna_unique, lat>(80)) #find northern midwest point
 points(22.01667,88.01667) #confirm this is northernmost point
-which(myna_points$lon == 22.01667, myna_points$lat== 88.01667) #find it in the data.frame
-myna_points<- myna_points[-145375,] #remove that row!
-myna_points<- myna_points[-145453,] #remove that row!
+which(myna_unique$lon == 22.01667, myna_unique$lat== 88.01667) #find it in the data.frame
+myna_unique<- myna_unique[-30626,] #remove that row!
+
+
+plot(wrld_simpl, axes=TRUE, col="light yellow", main = "Occurrences for Common Myna \n(Acridotheres tristis) from GBIF")
+points(myna_unique$lon, myna_unique$lat ,col = "blue", cex = .35)
 
 ####
 
@@ -53,7 +63,38 @@ myna_points<- myna_points[-145453,] #remove that row!
 
 worldclim <- getData('worldclim', var='bio', res=2.5)
 plot(worldclim[[1]], main = "Annual Mean Temp \n and Common Myna Occurrences")
-points(points(myna_points$lon, myna_points$lat ,col = "blue", cex = .25))
+points(points(myna_unique$lon, myna_unique$lat ,col = "blue", cex = .25))
 
 plot(worldclim[[19]], main = "Precip in Coldest Quarter \n and Common Myna Occurrences")
-points(points(myna_points$lon, myna_points$lat ,col = "blue", cex = .25))
+points(points(myna_unique$lon, myna_unique$lat ,col = "blue", cex = .25))
+
+#####
+
+# Spatial Thinning
+
+#####
+
+crs <- CRS('+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs') # set coordinate system
+
+myna_thin <-spThin( #thinning function
+  myna_unique, 
+  x.col = "lon",
+  y.col = "lat",
+  dist = 5000,
+  method= "gurobi", #can change to "gurobi" to make it even faster, but have to install it first
+  great.circle.distance=TRUE)
+summary(myna_thin)
+
+# Saving the thinned file####
+# print temporary dir
+print(tempdir())
+write.SpThin(
+  myna_thin,
+  coords=FALSE,
+  dir=tempdir()
+)
+#can elect to read in .csv of all thinned points
+thin_myna2<-read.csv("thin_0001.csv", head=T)
+head(thin_myna2)
+thin_ptw2_coords<-thin_myna2[,1:3]
+
