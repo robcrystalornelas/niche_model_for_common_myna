ad51dc0dfe96283d83b751b10dfb6271d0d23024
rpecchia <rpecchia@gmail.com>
2016-02-24 08:09:59 -0500

    bioclim model added

--- R/myna_enm.R
+++ R/myna_enm.R
@@ -11,6 +11,7 @@ library(dplyr)
 library(maptools)
 library(spThin)
 library(ggplot2)
+library(rgdal)
 data(wrld_simpl)
 
 setwd("/Users/rpecchia/Desktop/Zoogeo Spring 2016/zoogeo-project/R")
@@ -99,8 +100,11 @@ points(points(myna_unique$lon, myna_unique$lat ,col = "blue", cex = .25))
 plot(worldclim[[19]], main = "Precip in Coldest Quarter \n and Common Myna Occurrences")
 points(points(myna_unique$lon, myna_unique$lat ,col = "blue", cex = .25))
 
-<<<<<<< HEAD
-=======
+str(worldclim[[19]])
+#Pop density from SEDAC @ Columbia
+population<-raster(paste(getwd(), "/Pop_density_2000/pop2.tif", sep = ""))
+worldclim_and_pop <- stack(worldclim, population)
+
 #####
 
 # Spatial Thinning
@@ -129,6 +133,46 @@ write.SpThin(
 #can elect to read in .csv of all thinned points
 thin_myna2<-read.csv("thin_0001.csv", head=T)
 head(thin_myna2)
-thin_ptw2_coords<-thin_myna2[,1:3]
+thin_myna_coords<-thin_myna2[,2:3]
+dim(thin_myna_coords)
 
->>>>>>> 3b122253c90617ab409463e2d2d7e68c65d689f7
+#Making the BioClim Model
+??bioclim
+
+myna_bioclim<-bioclim(worldclim,thin_myna_coords)
+#this runs and saves the BIOCLIM model as an object
+myna_bioclim_predictions <- predict(myna_bioclim, worldclim, progress='text')
+plot(myna_bioclim_predictions)
+
+myna_bioclim_predictions_as_points <- rasterToPoints(myna_bioclim_predictions) #make predictions raster a set of points for ggplot
+df_myna_bioclim_predictions <- data.frame(myna_bioclim_predictions_as_points) #convert to data.frame
+head(df_myna_bioclim_predictions)
+colnames(df_myna_bioclim_predictions) <- c('lon', 'lat', 'Suitability') #Make appropriate column headings
+max(df_myna_bioclim_predictions$Suitability)
+
+map_bioclim_predictions <-ggplot(data=df_myna_bioclim_predictions, aes(y=lat, x=lon)) +
+  geom_raster(aes(fill=Suitability)) +
+  theme_bw() +
+  coord_equal() +
+  ggtitle("Common Myna BIOCLIM Model") +
+  theme(axis.title.x = element_text(size=16),
+        axis.title.y = element_text(size=16, angle=90),
+        axis.text.x = element_text(size=14),
+        axis.text.y = element_text(size=14),
+        plot.title = element_text(face="bold", size=20),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        legend.position = 'right',
+        legend.key = element_blank(),
+        panel.background = element_rect(fill = 'black')) +
+  #scale_fill_gradientn(colours=c("#ffffcc","#d9f0a3","#addd8e","#78c679","#41ab5d","#238443", "#005a32"), #green
+  #scale_fill_gradientn(colours=c("blue4","dodgerblue1","cyan1","darkolivegreen2","yellow1","darkorange1", "red"), na.value = "black",limits=c(0,.66))
+  scale_fill_gradientn(colours=c("#efedf5","dodgerblue1","cyan1","darkolivegreen2","yellow1","darkorange1", "red"), na.value = "black",limits=c(0,.66))
+  
+map_bioclim_predictions #full world map
+
+map_bioclim_predictions_nc_america<-map_bioclim_predictions + coord_fixed(xlim = c(-125.8,-62.2), ylim = c(3, 50)) #north/central america
+map_bioclim_predictions_nc_america
+map_bioclim_predictions_florida <- map_bioclim_predictions + coord_fixed(xlim = c(-88,-79), ylim = c(24, 32)) #florida
+map_bioclim_predictions_florida
+
